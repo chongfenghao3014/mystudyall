0527f375b2295ee28e579af6317687b19a101249
chongfenghao <572423417@qq.com>
2020-08-31 01:25:15 +0800

    无用改动比较代码

--- mystudyall/ZZZ-Test-SpringCloud/Test-Consumer/src/main/java/cn/aura/controller/B2BInOutProvider.java
+++ mystudyall/ZZZ-Test-SpringCloud/Test-Consumer/src/main/java/cn/aura/controller/B2BInOutProvider.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(value = "MICROCLOUD-PROVIDER-PRODUCT" ,path = "api")
-public interface B2BInOutProvider {
+public interface B2BInOutProvider {  //无用改动比较代码
     @RequestMapping("/product/get/{id}")// 虽然注解本身不支持继承，但是在spring中做了改变，子类可以共享接口的
                                         //  请求地址。 这里的path=“api”，那么请求路径就是 api/product/get/22
     public abstract Object getProduct(Long id);
