ecfe202516ede4ffa13540917c3fb502215d1926
chongfenghao02 <zk_cs_cs@163.com>
2020-09-15 23:21:45 +0800

    AAAAAAAAAAAAAAAAAAAAAA

--- mystudyall/ZZZ-TestA/src/main/java/cn/test/cn/test/testbranch/Test.java
+++ mystudyall/ZZZ-TestA/src/main/java/cn/test/cn/test/testbranch/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我是test分支代码");
         System.out.println("bbbbbbbbbbbbbbbbbbbbbbb");
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 }
