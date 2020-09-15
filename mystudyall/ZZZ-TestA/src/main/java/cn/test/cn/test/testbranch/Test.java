31eb8bcefe8881ae6c987f63bf9b54c3fc207bc6
chongfenghao02 <zk_cs_cs@163.com>
2020-09-15 23:34:39 +0800

    BBBBBBBBBBBBBBBBBBBBBBBBBBBB

--- mystudyall/ZZZ-TestA/src/main/java/cn/test/cn/test/testbranch/Test.java
+++ mystudyall/ZZZ-TestA/src/main/java/cn/test/cn/test/testbranch/Test.java
@@ -1,10 +1,17 @@
 package cn.test.cn.test.testbranch;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Test {
     private static String a = "a";
     private static String b = "a";
     private static String c = "a";
     private static String d = "a";
+
+    /*
+    * 哒哒哒哒哒哒多多多多多多多多多多多多多
+    * */
+    private static ThreadPoolExecutor pool = null;
     public static void main(String[] args) {
         System.out.println("我是test分支代码");
         System.out.println("cccccccccccccc");
