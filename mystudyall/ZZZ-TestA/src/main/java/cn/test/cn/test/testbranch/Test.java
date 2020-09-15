b21231df8451db2b710d08cc85c5b5670b34e271
chongfenghao02 <zk_cs_cs@163.com>
2020-09-15 23:18:30 +0800

    BBBBBBBBBBBBBBBBBBBBBBBBBBBB

--- mystudyall/ZZZ-TestA/src/main/java/cn/test/cn/test/testbranch/Test.java
+++ mystudyall/ZZZ-TestA/src/main/java/cn/test/cn/test/testbranch/Test.java
@@ -3,5 +3,6 @@ package cn.test.cn.test.testbranch;
 public class Test {
     public static void main(String[] args) {
         System.out.println("我是test分支代码");
+        System.out.println("bbbbbbbbbbbbbbbbbbbbbbb");
     }
 }
