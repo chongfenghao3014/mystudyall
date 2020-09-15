761951c5c56c07b4149a760ff3640628bafd74af
chongfenghao02 <zk_cs_cs@163.com>
2020-09-15 23:29:02 +0800

    BBBBBBBBBBBBBBBBBBBBBBBBBBBB

--- mystudyall/ZZZ-TestA/src/main/java/cn/test/cn/test/testbranch/Test.java
+++ mystudyall/ZZZ-TestA/src/main/java/cn/test/cn/test/testbranch/Test.java
@@ -7,7 +7,8 @@ public class Test {
     private static String d = "a";
     public static void main(String[] args) {
         System.out.println("我是test分支代码");
-        System.out.println("bbbbbbbbbbbbbbbbbbbbbbb");
+        System.out.println("cccccccccccccc");
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("dddddddddddddddddddddddddddddddd");
     }
 }
